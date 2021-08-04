c61642891391a4f1e6c9bbf04ccac2412fc56456
knakano <kei_nakano@sfu.ca>
2021-08-04 12:00:18 -0700

    Created Models for Game and PlayerSessionInfo and database insertion.

--- core/models.py
+++ core/models.py
@@ -6,9 +6,21 @@ class User(models.Model):
             return self.name
 
 
-class ZipFile(models.Model):
-    name=models.CharField(max_length=200)
-    data=models.FileField()
 
-    def __str__(self):
-            return self.name
+
+
+#Game[game_id, date, duration, platform, stage, victorCode]
+class Game(models.Model):
+    date=models.CharField(max_length=200)
+    duration=models.IntegerField()
+    platform=models.CharField(max_length=200)
+    stage =models.CharField(max_length=200)
+    victor= models.CharField(max_length=200)
+
+class PlayerSessionInfo(models.Model):
+    character=models.CharField(max_length=200)
+    color=models.IntegerField()
+    netplayCode=models.CharField(max_length=200)
+    name=models.CharField(max_length=200)
+    port= models.IntegerField()
+    game=models.ForeignKey(Game,on_delete=models.CASCADE)
--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from django.http import FileResponse
 from .models import *
 from .serializers import *
-from slippi import Game
+from slippi import Game as slippiGame #https://github.com/hohav/py-slippi
 from zipfile import ZipFile
 import os
 from django.core.files.storage import FileSystemStorage
@@ -41,11 +41,67 @@ def zip_upload(request):
 
 
         for i in fileList:
-            gameArray.append(Game(i))
+            gameArray.append(slippiGame(i))
 
+        #Game[game_id, date, duration, platform, stage, victorCode]
+        #game_id is unique key, victorCode being netplay code of winner
+
+        # playerSessionInfo[playerSessionInfoID,character, color, netplayCode,name, port, game_id]
+        # playerSessionInfoID is unqiueKey, game_id foreign key referencing Game
 
         for i in gameArray:
-            print(i)
+            dateInput = i.metadata.date
+            durationInput= i.metadata.duration
+            platformInput= i.metadata.platform
+            stageInput= i.start.stage
+            #To get victor, find at the last frame in game which player has the 0 or 1 stocks (Or 1 to account for ragequits, not 2 or more since most likely resets)
+
+            player0Stocks= i.frames[durationInput-1].ports[0].leader.post.stocks
+            player1Stocks=i.frames[durationInput-1].ports[1].leader.post.stocks
+
+
+            victorInput=""
+            #NOTE: Below code may break when not played on port 1 and 2, although I THINK Slippi auto places players into ports 1 and 2, I maybe wrong
+
+            #print(player0Stocks)
+            #print(player1Stocks)
+
+            if player0Stocks>player1Stocks:
+                victorInput=i.metadata.players[0].netplay.code
+            else:
+                victorInput=i.metadata.players[1].netplay.code
+
+
+            #Create new entry in Game table and insert
+            game=Game(date=dateInput,duration=durationInput,platform=platformInput,stage=stageInput,victor=victorInput)
+            game.save()
+
+            #print(victor)
+            #print(i.frames[durationInput-1].ports[1].leader.post.stocks)
+            game_id=game.id
+
+            char0= i.start.players[0].character
+            color0= i.start.players[0].costume
+            port0 = 0  #May be redundant, will store port though just in case
+            netplayCode0=i.metadata.players[0].netplay.code
+            name0= i.metadata.players[0].netplay.name
+
+            #Create new entry in PlayerSessionInfo table and insert appropriate session info
+            playerSessionInfo=PlayerSessionInfo(character=char0,color=color0,netplayCode=netplayCode0,name=name0,port=port0,game=game)
+            playerSessionInfo.save()
+
+            char1= i.start.players[1].character
+            color1= i.start.players[1].costume
+            port1 = 1  #May be redundant, will store port though just in case
+            netplayCode1=i.metadata.players[1].netplay.code
+            name1= i.metadata.players[1].netplay.name
+
+            playerSessionInfo=PlayerSessionInfo(character=char1,color=color1,netplayCode=netplayCode1,name=name1,port=port1,game=game)
+            playerSessionInfo.save()
+
+        print(Game.objects.values())
+        print(PlayerSessionInfo.objects.values())
+
 
         print('Done!')
 
